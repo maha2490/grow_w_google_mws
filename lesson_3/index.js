62626bbb6e555117f9f2310c4133a213df275c48
Rachel Mahan <rachelm2490@gmail.com>
2018-01-26 22:40:08 -0500

    answer 3.17

---
+++ lesson_3/index.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', function(event) {
+  var urlsToCache = [
+    '/',
+    'js/main.js',
+    'css/main.css',
+    'imgs/icon.png',
+    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+  ];
+
+  event.waitUntil(
+    caches.open('wittr-static-v1').then(function(cache){
+      // returns a promise. no implied return
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
