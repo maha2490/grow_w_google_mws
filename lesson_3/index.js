edd15a1a668ba32b5c567037090486aa40c2d1a1
Rachel Mahan <rachelm2490@gmail.com>
2018-01-28 08:57:40 -0500

    answer 3.18

--- lesson_3/index.js
+++ lesson_3/index.js
@@ -1,17 +1,29 @@
 self.addEventListener('install', function(event) {
-  var urlsToCache = [
-    '/',
-    'js/main.js',
-    'css/main.css',
-    'imgs/icon.png',
-    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-  ];
-
   event.waitUntil(
-    caches.open('wittr-static-v1').then(function(cache){
-      // returns a promise. no implied return
-      return cache.addAll(urlsToCache);
+    caches.open('wittr-static-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ]);
     })
   );
+});
+
+self.addEventListener('fetch', function(event) {
+  // needs to be called asynchronously, outside of promise
+  event.respondWith(
+    // if there is a cache, return that
+    // else fetch from the network
+    caches.match(event.request).then(function(response){
+      if(response){
+        return response;
+      } else {
+        return fetch(event.request);
+      }
+    })
+  )
 });
